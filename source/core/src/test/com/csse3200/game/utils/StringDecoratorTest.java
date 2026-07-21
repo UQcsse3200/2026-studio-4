5704ffa9adc60c59299f055a59633b594a01c851
Jackson Trenerry <105094182+JTrenerry@users.noreply.github.com>
2026-07-21 13:04:14 +1000

    Initial commit

---
+++ source/core/src/test/com/csse3200/game/utils/StringDecoratorTest.java
@@ -0,0 +1,30 @@
+package com.csse3200.game.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.csse3200.game.extensions.GameExtension;
+import java.util.function.Function;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(GameExtension.class)
+class StringDecoratorTest {
+  @Test
+  void shouldReturnGivenString() {
+    Function<Integer, String> customToString = (Integer n) -> "hello";
+    StringDecorator<Integer> decorator = new StringDecorator<>(5, customToString);
+
+    assertEquals("hello", decorator.toString());
+  }
+
+  @Test
+  void shouldPassCorrectObject() {
+    Function<Integer, String> customToString = (Integer n) -> Integer.toString(n * 2);
+
+    StringDecorator<Integer> decorator = new StringDecorator<>(5, customToString);
+    assertEquals("10", decorator.toString());
+
+    decorator = new StringDecorator<>(10, customToString);
+    assertEquals("20", decorator.toString());
+  }
+}
