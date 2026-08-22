37cc0beea95cbf573da3f7e1aa42a4d60dec6da4
Kelvin Hsu <khsu7khsu@gmail.com>
2026-08-22 15:12:02 +1000

    Added java docs

--- source/core/src/main/com/csse3200/game/entities/factories/RoomFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/RoomFactory.java
@@ -5,7 +5,17 @@ import com.csse3200.game.areas.terrain.TerrainFactory;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.rendering.Renderer;
 
+/** Factory to create rooms with predefined components */
 public class RoomFactory {
+
+  /**
+   * Creates the original forest game area as a entity.
+   *
+   * <p>Can be removed later.
+   *
+   * @param renderer
+   * @return
+   */
   public static Entity createForestGameArea(Renderer renderer) {
     TerrainFactory terrain = new TerrainFactory(renderer.getCamera());
     Entity entity = new Entity().addComponent(new ForestGameArea(terrain));
