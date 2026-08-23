ebb461445c972b8d664a668cdcaebe3404453eed
Kelvin Hsu <khsu7khsu@gmail.com>
2026-08-23 15:15:05 +1000

    Created RoomAssetsComponent

---
+++ source/core/src/main/com/csse3200/game/components/rooms/RoomAssetsComponent.java
@@ -0,0 +1,83 @@
+package com.csse3200.game.components.rooms;
+
+import com.badlogic.gdx.audio.Music;
+import com.csse3200.game.areas.terrain.TerrainConfig;
+import com.csse3200.game.components.Component;
+import com.csse3200.game.services.ResourceService;
+import com.csse3200.game.services.ServiceLocator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Loads the provided textures into the resourceService. Any textures used within the room should
+ * exist here. Different rooms may use different textures.
+ */
+public class RoomAssetsComponent extends Component {
+  private static final Logger logger = LoggerFactory.getLogger(RoomAssetsComponent.class);
+  private final String playerTexture = "images/box_boy_leaf.png";
+  private final String tileTexture1 = "images/grass_1.png";
+  private final String tileTexture2 = "images/grass_2.png";
+  private final String tileTexture3 = "images/grass_3.png";
+
+  private final String backgroundMusic = "sounds/BGM_03_mp3.mp3";
+
+  private final String[] textures = {playerTexture, tileTexture1, tileTexture2, tileTexture3};
+  private final String[] music = {backgroundMusic};
+  private final String[] sounds = {"sounds/Impact4.ogg"};
+  private final String[] enemyTextures = {"images/ghost_1.png", "images/ghost_king.png"};
+  private final String[] enemyTextureAtlases = {
+    "images/terrain_iso_grass.atlas", "images/ghost.atlas", "images/ghostKing.atlas"
+  };
+
+  @Override
+  public void create() {
+    playMusic();
+  }
+
+  @Override
+  public void dispose() {
+    ServiceLocator.getResourceService().getAsset(backgroundMusic, Music.class).stop();
+    unloadAssets();
+  }
+
+  public RoomAssetsComponent() {
+    loadAssets();
+  }
+
+  public void setTerrainConfig(TerrainConfig terrainConfig) {
+    terrainConfig.setTextures(tileTexture1, tileTexture2, tileTexture3);
+  }
+
+  private void loadAssets() {
+    logger.debug("loading assets");
+    final ResourceService resourceService = ServiceLocator.getResourceService();
+    resourceService.loadTextures(textures);
+    resourceService.loadTextures(enemyTextures);
+    resourceService.loadTextureAtlases(enemyTextureAtlases);
+
+    resourceService.loadMusic(music);
+    resourceService.loadSounds(sounds);
+
+    while (!resourceService.loadForMillis(10)) {
+      // This could be upgraded to a loading screen
+      logger.info("Loading... {}%", resourceService.getProgress());
+    }
+  }
+
+  private void playMusic() {
+    final Music music = ServiceLocator.getResourceService().getAsset(backgroundMusic, Music.class);
+    music.setLooping(true);
+    music.setVolume(0.3f);
+    music.play();
+  }
+
+  private void unloadAssets() {
+    logger.debug("unloading assets");
+    final ResourceService resourceService = ServiceLocator.getResourceService();
+    resourceService.unloadAssets(textures);
+    resourceService.unloadAssets(enemyTextures);
+    resourceService.unloadAssets(enemyTextureAtlases);
+    resourceService.unloadAssets(music);
+    resourceService.unloadAssets(sounds);
+  }
+}
