2669d35ba73ff4727ea0b3f5f6ea2ca4f9e4ee50
edenyzh <edenyzh@163.com>
2026-08-23 23:46:26 +1000

    chore: apply google java format

--- source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
+++ source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
@@ -81,7 +81,6 @@ public class CombatStatsComponent extends Component {
       entity.getEvents().trigger("updateHealth", this.health);
       if (!wasDead && isDead()) {
         entity.getEvents().trigger("enemyDied", entity.getPosition(), entity);
-
       }
     }
   }
