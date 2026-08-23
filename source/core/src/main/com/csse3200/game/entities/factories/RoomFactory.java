d50d319a2cf0580d9daa310fe68d484fc43ef20c
Kelvin Hsu <khsu7khsu@gmail.com>
2026-08-23 15:20:15 +1000

    Modified MainGameScreen to use new Room entity

--- source/core/src/main/com/csse3200/game/entities/factories/RoomFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/RoomFactory.java
@@ -1,7 +1,10 @@
 package com.csse3200.game.entities.factories;
 
 import com.csse3200.game.areas.ForestGameArea;
+import com.csse3200.game.areas.terrain.TerrainConfig;
 import com.csse3200.game.areas.terrain.TerrainFactory;
+import com.csse3200.game.components.gamearea.GameAreaDisplay;
+import com.csse3200.game.components.rooms.RoomAssetsComponent;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.rendering.Renderer;
 
@@ -19,6 +22,24 @@ public class RoomFactory {
   public static Entity createForestGameArea(Renderer renderer) {
     TerrainFactory terrain = new TerrainFactory(renderer.getCamera());
     Entity entity = new Entity().addComponent(new ForestGameArea(terrain));
+    return entity;
+  }
+
+  public static Entity createRoom(Renderer renderer, String roomName) {
+    TerrainFactory terrainFactory = new TerrainFactory(renderer.getCamera());
+    TerrainConfig terrainConfig = new TerrainConfig();
+    terrainConfig.gAltTexture1Count = 100;
+    terrainConfig.gAltTexture2Count = 80;
+
+    RoomAssetsComponent assetsComponent = new RoomAssetsComponent();
+    assetsComponent.setTerrainConfig(terrainConfig);
+
+    Entity entity =
+        new Entity()
+            .addComponent(assetsComponent)
+            .addComponent(new GameAreaDisplay(roomName))
+            .addComponent(terrainFactory.createTerrain(terrainConfig, renderer.getCamera()));
+
     return entity;
   }
 }
