e7fbda718b076d637f18f3d0f9e8f1f1cc284b39
edenyzh <edenyzh@163.com>
2026-08-24 01:01:44 +1000

    Add unit test for DeathComponent disposal on enemyDied event

---
+++ source/core/src/test/com/csse3200/game/components/DeathComponentTest.java
@@ -0,0 +1,28 @@
+package com.csse3200.game.components;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import com.badlogic.gdx.math.Vector2;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.EntityService;
+import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.services.ServiceLocator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(GameExtension.class)
+class DeathComponentTest {
+  @Test
+  void shouldDisposeEntityOnDeathEvent() {
+    EntityService entityService = mock(EntityService.class);
+    ServiceLocator.registerEntityService(entityService);
+
+    Entity entity = new Entity().addComponent(new DeathComponent());
+    entity.create();
+
+    entity.getEvents().trigger("enemyDied", new Vector2(0, 0), entity);
+
+    verify(entityService).unregister(entity);
+  }
+}
