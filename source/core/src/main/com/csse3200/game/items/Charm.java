a59ecb7823312c5644fc8db60c114684dc6c1845
Sumith Bagalkot <sumithbagalkoti@gmail.com>
2026-08-24 16:08:17 +1000

    style: format Charm model

--- source/core/src/main/com/csse3200/game/items/Charm.java
+++ source/core/src/main/com/csse3200/game/items/Charm.java
@@ -1,16 +1,14 @@
 package com.csse3200.game.items;
 
-/**
- * Represents a charm item that can be stored in the player's inventory.
- */
+/** Represents a charm item that can be stored in the player's inventory. */
 public class Charm {
-    private final String name;
+  private final String name;
 
-    public Charm(String name) {
-        this.name = name;
-    }
+  public Charm(String name) {
+    this.name = name;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 }
