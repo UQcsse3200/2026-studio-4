4ca8acc122949f21b05469ea79accd09a36da0d7
Wouter Teunisse <wouterteunisse1002@icloud.com>
2026-08-28 10:26:03 +1000

    Created hitbox spec for SwordWeaponComponent

--- source/core/src/main/com/csse3200/game/components/weapons/SwordWeaponComponent.java
+++ source/core/src/main/com/csse3200/game/components/weapons/SwordWeaponComponent.java
@@ -1,6 +1,11 @@
 package com.csse3200.game.components.weapons;
 
 import com.badlogic.gdx.math.Vector2;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.factories.HitboxFactory;
+import com.csse3200.game.entities.factories.HitboxSpec;
+import com.csse3200.game.physics.PhysicsLayer;
+import com.csse3200.game.services.ServiceLocator;
 
 /**
  * Slower sweeping attack. Spawns a short range radiant hitbox just in front of the wielder that
@@ -12,5 +17,23 @@ public class SwordWeaponComponent extends WeaponComponent {
   private static final float REACH = 0.5f;
 
   @Override
-  protected void createAttack(Vector2 origin, Vector2 direction) {}
+  protected void createAttack(Vector2 origin, Vector2 direction) {
+      WeaponStatsComponent stats = entity.getComponent(WeaponStatsComponent.class);
+      Vector2 offset = direction.cpy().nor().scl(REACH);
+
+      HitboxSpec spec =
+          new HitboxSpec()
+              .position(origin)
+              .size(SIZE)
+              .lifetime(LIFETIME)
+              .layer(PhysicsLayer.WEAPON)
+              .targetLayer(PhysicsLayer.NPC)
+              .damage(stats.getDamage())
+              .knockback(stats.getKnockback())
+              .owner(entity)
+              .localOffset(offset);
+
+      Entity hitbox = HitboxFactory.createHitbox(spec);
+      ServiceLocator.getEntityService().register(hitbox);
+  }
 }
