807d95110b85ea4b2471c66ae3549e0aee1bee6c
edenyzh <edenyzh@163.com>
2026-08-29 17:44:33 +1000

    Align DeathComponent listener with parameterless entityDied event and update test

--- source/core/src/main/com/csse3200/game/components/EnemyDeathComponent.java
+++ source/core/src/main/com/csse3200/game/components/EnemyDeathComponent.java
@@ -1,8 +1,5 @@
 package com.csse3200.game.components;
 
-import com.badlogic.gdx.math.Vector2;
-import com.csse3200.game.entities.Entity;
-
 /**
  * Listens for the enemy's death event and disposes the entity when it fires. This keeps death
  * handling separate from health tracking so that CombatStatsComponent does not need to dispose
@@ -16,7 +13,7 @@ public class EnemyDeathComponent extends Component {
         .getEvents()
         .addListener(
             "entityDied",
-            (Vector2 position, Entity deadEntity) -> {
+            () -> {
               entity.dispose();
             });
   }
