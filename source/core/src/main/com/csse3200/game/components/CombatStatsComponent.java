b3641fa7f7f089daf9f956f978d5975d7eaed2c8
edenyzh <edenyzh@163.com>
2026-08-30 14:48:24 +1000

    Fix entityDied event argument mismatch

--- source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
+++ source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
@@ -81,7 +81,7 @@ public class CombatStatsComponent extends Component {
     if (entity != null) {
       entity.getEvents().trigger("updateHealth", this.health);
       if (!wasDead && isDead()) {
-        entity.getEvents().trigger("entityDied", this.getEntity());
+        entity.getEvents().trigger("entityDied");
       }
     }
   }
