e949f68c29d71b5b44cf142e2188d67d0ca46412
jas <hey82359@users.noreply.github.com>
2026-08-30 23:14:53 +1000

    Add projectile collision damage

--- source/core/src/main/com/csse3200/game/entities/factories/FloatingDemonProjectileFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/FloatingDemonProjectileFactory.java
@@ -4,16 +4,17 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 import com.csse3200.game.components.npc.ProjectileMovementComponent;
-import com.csse3200.game.components.weapons.LifetimeComponent;
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.physics.PhysicsLayer;
 import com.csse3200.game.rendering.AnimationRenderComponent;
 import com.csse3200.game.services.ServiceLocator;
 
-/** Creates the visual projectiles fired by a floating demon. */
+/** Creates the projectiles fired by a floating demon. */
 public class FloatingDemonProjectileFactory {
   private static final float PROJECTILE_SPEED = 5f;
   private static final float PROJECTILE_RANGE = 7f;
   private static final float PROJECTILE_LIFETIME = 1.8f;
+  private static final int PROJECTILE_DAMAGE = 5;
 
   public static Entity createProjectile(Vector2 position, Vector2 direction) {
     TextureAtlas atlas =
@@ -23,18 +24,24 @@ public class FloatingDemonProjectileFactory {
     animator.addAnimation("projectile", 0.1f, Animation.PlayMode.LOOP);
     animator.addAnimation("projectileHit", 0.1f);
 
-    Entity projectile =
-        new Entity()
-            .addComponent(
-                new ProjectileMovementComponent(direction, PROJECTILE_SPEED, PROJECTILE_RANGE))
-            .addComponent(new LifetimeComponent(PROJECTILE_LIFETIME))
-            .addComponent(animator);
+    HitboxSpec spec =
+        new HitboxSpec()
+            .position(position)
+            .size(new Vector2(0.8f, 0.8f))
+            .lifetime(PROJECTILE_LIFETIME)
+            .layer(PhysicsLayer.WEAPON)
+            .targetLayer(PhysicsLayer.PLAYER)
+            .damage(PROJECTILE_DAMAGE);
+
+    Entity projectile = HitboxFactory.createHitbox(spec);
+    projectile
+        .addComponent(
+            new ProjectileMovementComponent(direction, PROJECTILE_SPEED, PROJECTILE_RANGE))
+        .addComponent(animator);
 
     projectile
         .getEvents()
         .addListener("projectileRangeReached", () -> animator.startAnimation("projectileHit"));
-    projectile.setPosition(position);
-    projectile.setScale(0.8f, 0.8f);
     animator.startAnimation("projectile");
     return projectile;
   }
