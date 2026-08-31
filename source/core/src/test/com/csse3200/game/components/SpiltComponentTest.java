3e1f3756d83aae68514bc99c01281aab53452966
Shravika Mittal <mittalshravika10@gmail.com>
2026-08-31 12:02:39 +1000

    Add unit tests for LungeAttackTask

--- source/core/src/test/com/csse3200/game/components/SpiltComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/SpiltComponentTest.java
@@ -19,69 +19,69 @@ import org.mockito.MockedStatic;
 @ExtendWith(GameExtension.class)
 class SpiltComponentTest {
 
-    private MockedStatic<NPCFactory> npcFactoryMock;
-    private EntityService entityService;
+  private MockedStatic<NPCFactory> npcFactoryMock;
+  private EntityService entityService;
 
-    @BeforeEach
-    void setUp() {
-        entityService = mock(EntityService.class);
-        ServiceLocator.registerEntityService(entityService);
-        npcFactoryMock = mockStatic(NPCFactory.class);
-    }
+  @BeforeEach
+  void setUp() {
+    entityService = mock(EntityService.class);
+    ServiceLocator.registerEntityService(entityService);
+    npcFactoryMock = mockStatic(NPCFactory.class);
+  }
 
-    @AfterEach
-    void tearDown() {
-        npcFactoryMock.close();
-    }
+  @AfterEach
+  void tearDown() {
+    npcFactoryMock.close();
+  }
 
-    @Test
-    void shouldSplitIntoTwoOnFirstHit() {
-        Entity target = new Entity();
+  @Test
+  void shouldSplitIntoTwoOnFirstHit() {
+    Entity target = new Entity();
 
-        Entity original = new Entity();
-        CombatStatsComponent stats = new CombatStatsComponent(40, 10);
-        original.addComponent(stats);
-        original.addComponent(new SpiltComponent(target));
-        original.create();
+    Entity original = new Entity();
+    CombatStatsComponent stats = new CombatStatsComponent(40, 10);
+    original.addComponent(stats);
+    original.addComponent(new SpiltComponent(target));
+    original.create();
 
-        Entity childStub = new Entity();
-        childStub.addComponent(new CombatStatsComponent(1, 1));
-        npcFactoryMock.when(() -> NPCFactory.createChaseEnemy(target)).thenReturn(childStub);
+    Entity childStub = new Entity();
+    childStub.addComponent(new CombatStatsComponent(1, 1));
+    npcFactoryMock.when(() -> NPCFactory.createChaseEnemy(target)).thenReturn(childStub);
 
-        original.getEvents().trigger("hitReaction", target);
+    original.getEvents().trigger("hitReaction", target);
 
-        verify(entityService, times(2)).register(childStub);
-    }
+    verify(entityService, times(2)).register(childStub);
+  }
 
-    @Test
-    void shouldOnlySplitOnce() {
-        Entity target = new Entity();
+  @Test
+  void shouldOnlySplitOnce() {
+    Entity target = new Entity();
 
-        Entity original = new Entity();
-        original.addComponent(new CombatStatsComponent(40, 10));
-        original.addComponent(new SpiltComponent(target));
-        original.create();
+    Entity original = new Entity();
+    original.addComponent(new CombatStatsComponent(40, 10));
+    original.addComponent(new SpiltComponent(target));
+    original.create();
 
-        Entity childStub = new Entity();
-        childStub.addComponent(new CombatStatsComponent(1, 1));
-        npcFactoryMock.when(() -> NPCFactory.createChaseEnemy(target)).thenReturn(childStub);
+    Entity childStub = new Entity();
+    childStub.addComponent(new CombatStatsComponent(1, 1));
+    npcFactoryMock.when(() -> NPCFactory.createChaseEnemy(target)).thenReturn(childStub);
 
-        original.getEvents().trigger("hitReaction", target);
-        original.getEvents().trigger("hitReaction", target);
+    original.getEvents().trigger("hitReaction", target);
+    original.getEvents().trigger("hitReaction", target);
 
-        verify(entityService, times(2)).register(childStub);
-    }
+    verify(entityService, times(2)).register(childStub);
+  }
 
-    @Test
-    void shouldDoNothingIfNoCombatStatsComponent() {
-        Entity target = new Entity();
+  @Test
+  void shouldDoNothingIfNoCombatStatsComponent() {
+    Entity target = new Entity();
 
-        Entity original = new Entity();
-        original.addComponent(new SpiltComponent(target));
-        original.create();
+    Entity original = new Entity();
+    original.addComponent(new SpiltComponent(target));
+    original.create();
 
-        original.getEvents().trigger("hitReaction", target);
+    original.getEvents().trigger("hitReaction", target);
 
-        npcFactoryMock.verifyNoInteractions();
-    }
+    npcFactoryMock.verifyNoInteractions();
+  }
 }
