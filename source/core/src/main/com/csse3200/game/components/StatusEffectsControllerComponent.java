cd4e75650a028950d23cbfbeadaacd51cb3aad18
Clancy Young <clancyry07@gmail.com>
2026-09-06 09:51:38 +1000

    Add comments. Add check that burning stacks is positive.

--- source/core/src/main/com/csse3200/game/components/StatusEffectsControllerComponent.java
+++ source/core/src/main/com/csse3200/game/components/StatusEffectsControllerComponent.java
@@ -11,6 +11,11 @@ public class StatusEffectsControllerComponent extends Component {
   private long lastBurn;
   private final long BURN_COOLDOWN = 1000;
 
+  /**
+   * Caches the CombatStatsComponent from entity.
+   *
+   * <p>Throws IllegalStateException if CombatStatsComponent is null.
+   */
   public void create() {
     combatStatsComponent = entity.getComponent(CombatStatsComponent.class);
     if (combatStatsComponent == null) {
@@ -19,13 +24,25 @@ public class StatusEffectsControllerComponent extends Component {
     }
   }
 
+  /**
+   * Adds stacks to burning. Throws IllegalArgumentException if stacks <= 0.
+   *
+   * @param stacks the number of stacks of burning to add.
+   */
   public void burningOn(int stacks) {
+    if (stacks <= 0) {
+      throw new IllegalArgumentException("Stacks of burning must be > 0");
+    }
     if (burning == 0) {
       lastBurn = time.getTime();
     }
     burning += stacks;
   }
 
+  /**
+   * Updates the state of all status effects. Burning: If time since last burn > burn cooldown,
+   * deals 1 damage per stack of burn.
+   */
   public void update() {
     if (burning > 0 && time.getTimeSince(lastBurn) > BURN_COOLDOWN) {
       combatStatsComponent.takeDamage(burning); // Deals 1 damage per stack of burning.
