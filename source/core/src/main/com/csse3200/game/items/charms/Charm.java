645da23c3dc0d33afff83d2dbf859d0c6be1e481
Kelvin Hsu <khsu7khsu@gmail.com>
2026-09-11 15:28:20 +1000

    Removed unused import
    Fixes compiler error

--- source/core/src/main/com/csse3200/game/items/charms/Charm.java
+++ source/core/src/main/com/csse3200/game/items/charms/Charm.java
@@ -3,7 +3,6 @@ package com.csse3200.game.items.charms;
 import com.csse3200.game.components.player.InventoryComponent;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.items.Item;
-import com.csse3200.game.items.Pickupable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
