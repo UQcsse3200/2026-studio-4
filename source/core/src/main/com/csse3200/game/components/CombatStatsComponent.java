d3075a3bc4a45df5e886a390895061f1740ebf1e
Kelvin Hsu <khsu7khsu@gmail.com>
2026-09-15 11:32:25 +1000

    Added scaling functionality

--- source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
+++ source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
@@ -20,6 +20,8 @@ public class CombatStatsComponent extends Component {
   private boolean invulnerable;
   private float incomingDamageMultiplier = 1f;
   private int minimumHealth;
+  private float attackScaleFactor = 0.5f;
+  private float healthScaleFactor = 0.5f;
 
   public CombatStatsComponent(int health, int baseAttack) {
     this.maxHealth = health;
@@ -33,6 +35,28 @@ public class CombatStatsComponent extends Component {
     setBaseAttack(baseAttack);
     setMovementSpeed(movementSpeed);
     setAttackSpeed(attackSpeed);
+  }
+
+  /**
+   * Calls scale on maxHealth and baseAttack with their respective scale factors
+   *
+   * @param amount The amount of times scaling is applied
+   */
+  public void scale(int amount) {
+    maxHealth += scaleStat(maxHealth, healthScaleFactor, amount);
+    baseAttack += scaleStat(baseAttack, attackScaleFactor, amount);
+  }
+
+  /**
+   * Returns the amount to increase a stat by when scaling it.
+   *
+   * @param stat The base stat to scale off
+   * @param factor The factor to scale by
+   * @param amount The amount of times scaling is applied
+   * @return Amount to increate stat by
+   */
+  private int scaleStat(int stat, float factor, int amount) {
+    return (int) (stat * factor * amount);
   }
 
   /**
