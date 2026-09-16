49a51f5ef9787e2fd1e49c83e20b79fa5c2ac38c
Wouter Teunisse <wouterteunisse1002@icloud.com>
2026-09-16 13:35:53 +1000

    Added support for targeting in custom radius + stylecheck

--- source/core/src/main/com/csse3200/game/components/abilities/targeting/StrategyWithinRadius.java
+++ source/core/src/main/com/csse3200/game/components/abilities/targeting/StrategyWithinRadius.java
@@ -5,25 +5,42 @@ import com.badlogic.gdx.utils.Array;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.services.ServiceLocator;
 
-public class StrategyWithinRadius implements EnemyTargetingStrategy{
+public class StrategyWithinRadius implements EnemyTargetingStrategy {
 
-    public Array<Entity> selectTargets(Entity caster) {
-        Vector2 casterCenter = caster.getCenterPosition();
-        float radius = 10f;
-        float radiusSq = radius * radius;
-        Array<Entity> targets = new Array<>();
+  public Array<Entity> selectTargets(Entity caster) {
+    Vector2 casterCenter = caster.getCenterPosition();
+    float radius = 10f;
+    float radiusSq = radius * radius;
+    Array<Entity> targets = new Array<>();
 
-        for (Entity candidate: ServiceLocator.getEntityService().getEntities()) {
-            if (candidate.equals(caster) || !EnemyUtils.isEnemy(candidate)) {
-                continue;
-            }
-            float distSq = candidate.getCenterPosition().dst2(casterCenter);
-            if (distSq < radiusSq) {
-                targets.add(candidate);
-            }
-        }
-
-        return targets;
+    for (Entity candidate : ServiceLocator.getEntityService().getEntities()) {
+      if (candidate.equals(caster) || !EnemyUtils.isEnemy(candidate)) {
+        continue;
+      }
+      float distSq = candidate.getCenterPosition().dst2(casterCenter);
+      if (distSq < radiusSq) {
+        targets.add(candidate);
+      }
     }
 
+    return targets;
+  }
+
+  public Array<Entity> selectTargets(Entity caster, float radius) {
+    Vector2 casterCenter = caster.getCenterPosition();
+    float radiusSq = radius * radius;
+    Array<Entity> targets = new Array<>();
+
+    for (Entity candidate : ServiceLocator.getEntityService().getEntities()) {
+      if (candidate.equals(caster) || !EnemyUtils.isEnemy(candidate)) {
+        continue;
+      }
+      float distSq = candidate.getCenterPosition().dst2(casterCenter);
+      if (distSq < radiusSq) {
+        targets.add(candidate);
+      }
+    }
+
+    return targets;
+  }
 }
