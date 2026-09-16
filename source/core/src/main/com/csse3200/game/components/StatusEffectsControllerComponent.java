18737facdfada1680fc4d3508a956fe6a423439a
krish-uq <krishtech4us@gmail.com>
2026-09-16 13:57:23 +1000

    remove keybinds for shield and events associated with it.

--- source/core/src/main/com/csse3200/game/components/StatusEffectsControllerComponent.java
+++ source/core/src/main/com/csse3200/game/components/StatusEffectsControllerComponent.java
@@ -123,8 +123,8 @@ public class StatusEffectsControllerComponent extends Component {
       throw new IllegalStateException(
           "StatusEffectsController requires CombatStatsComponent on the same entity.");
     }
-    entity.getEvents().addListener("shieldAbsorb", this::activateAbsorb);
-    entity.getEvents().addListener("shieldTimed", this::activateTimed);
+//    entity.getEvents().addListener("shieldAbsorb", this::activateAbsorb);
+//    entity.getEvents().addListener("shieldTimed", this::activateTimed);
     entity.getEvents().addListener("entityDied", this::clearStatusEffects);
   }
 
--- source/core/src/main/com/csse3200/game/components/player/KeyboardPlayerInputComponent.java
+++ source/core/src/main/com/csse3200/game/components/player/KeyboardPlayerInputComponent.java
@@ -71,12 +71,6 @@ public class KeyboardPlayerInputComponent extends InputComponent {
         entity.getEvents().trigger("interact");
         entity.getEvents().trigger("itemPickup");
         return true;
-      case Keys.NUM_0:
-        entity.getEvents().trigger("shieldAbsorb");
-        return true;
-      case Keys.NUM_9:
-        entity.getEvents().trigger("shieldTimed");
-        return true;
       case Keys.I:
         entity.getComponent(InventoryComponent.class).toggleDisplay();
         return true;
