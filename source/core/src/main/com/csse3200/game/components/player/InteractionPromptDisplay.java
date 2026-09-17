1dd18234bd6b10c1f1b3b2f4513b0f6483bd77c8
krish-uq <krishtech4us@gmail.com>
2026-09-17 12:27:47 +1000

    fix positioning

--- source/core/src/main/com/csse3200/game/components/player/InteractionPromptDisplay.java
+++ source/core/src/main/com/csse3200/game/components/player/InteractionPromptDisplay.java
@@ -22,12 +22,12 @@ public class InteractionPromptDisplay extends UIComponent {
     super.create();
     table = new Table();
     table.setFillParent(true);
-    table.align(Align.bottom | Align.center);
-    table.padBottom(20f).padRight(20f);
+    table.align(Align.top | Align.right);
+    table.padTop(50f).padRight(20f);
 
     promptLabel = new Label("", skin, "statDisplay");
     promptLabel.setColor(Color.WHITE);
-    promptLabel.setFontScale(1.15f);
+    promptLabel.setFontScale(1f);
 
     bubble = new Table();
     bubble.setBackground(skin.getDrawable("tooltip"));
